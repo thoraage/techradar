0fa14733c23c3ba993e698c7d42232c5f60c3884
Thor Åge Eldby <tel@knowit.no>
2022-06-22 20:09:08 +0200

    Added search for technologies

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,5 @@
 <script>
-    import { Form, FormGroup, FormText, Input, Label } from 'sveltestrap';
+    import {Form, FormGroup, FormText, Input, Label, ListGroup, ListGroupItem} from 'sveltestrap';
     import {ApolloClient, InMemoryCache, HttpLink} from '@apollo/client';
 
     const client = new ApolloClient({
@@ -16,41 +16,50 @@
     // setClient(client);
 
     const FIELDS = gql`query { technology_fields { id name description } }`;
-    const fieldsQuery = client.query({ query: FIELDS });
-    console.log(fieldsQuery);
-    fieldsQuery.then(response => {
-        console.log(response);
-    });
+    const fieldsQuery = client.query({ query: FIELDS }).then(r => r.data.technology_fields);
     fieldsQuery.catch(reason => {
         console.log("What happened: " + reason);
     })
-    $: fields = fieldsQuery;
-    $: arrived = fieldsQuery.data ? true : false;
-    let name = "Biden";
+
+    $: technologiesQuery = Promise.allSettled([]);
+
+    function searchTechnology(search) {
+        if (search.length >= 1) {
+            const TECHNOLOGIES = gql`query ($search: String!) { technologies(where: { name: { _ilike: $search }}) { id name } }`;
+            let variables = {search: "%" + search + "%"};
+            technologiesQuery = client.query({
+                query: TECHNOLOGIES,
+                variables: variables
+            }).then(r => r.data.technologies);
+            technologiesQuery.then(console.log);
+        } else {
+            technologiesQuery = Promise.allSettled([]);
+        }
+    }
 </script>
+
+<h1>Tech Radar</h1>
+<Form>
+    <FormGroup>
+        <Input type="select">
+            {#await fieldsQuery then fields}
+                {#each fields as field}
+                    <option>{field.name}</option>
+                {/each}
+            {/await}
+        </Input>
+        <Input type="text" on:input={event => searchTechnology(event.target.value)}/>
+        <ListGroup>
+            {#await technologiesQuery then technologies}
+                {#each technologies as technology}
+                    <ListGroupItem>{technology.name}</ListGroupItem>
+                {/each}
+            {/await}
+        </ListGroup>
+    </FormGroup>
+</Form>
 
 <style>
     @import "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css";
 </style>
 
-<h1>Let's go {arrived}</h1>
-
-<Form>
-    <FormGroup>
-        <Input type="select" name="select" id="exampleSelect">
-            {#await fields}
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>
-            {:then f}
-                {#each f.data.technology_fields as field}
-                    <option>{field.name}</option>
-                {/each}
-            {:catch error}
-                Don't know what to say: {error}
-            {/await}
-        </Input>
-    </FormGroup>
-</Form>
