5b317283cd0cd8dde528c24d9b0cd4b886dd5f60
Thor Åge Eldby <tel@knowit.no>
2022-06-22 22:37:20 +0200

    Shined up a tad

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,15 @@
 <script>
-    import {Form, FormGroup, FormText, Input, Label, ListGroup, ListGroupItem} from 'sveltestrap';
-    import {ApolloClient, InMemoryCache, HttpLink} from '@apollo/client';
+    import {Col, Row, Styles} from 'sveltestrap';
+    import {
+        Form,
+        FormGroup, Icon,
+        Input,
+        InputGroup,
+        InputGroupText,
+        ListGroup,
+        ListGroupItem
+    } from 'sveltestrap';
+    import {ApolloClient, InMemoryCache} from '@apollo/client';
 
     const client = new ApolloClient({
         uri: 'http://localhost:8080/v1/graphql',
@@ -17,9 +26,6 @@
 
     const FIELDS = gql`query { technology_fields { id name description } }`;
     const fieldsQuery = client.query({ query: FIELDS }).then(r => r.data.technology_fields);
-    fieldsQuery.catch(reason => {
-        console.log("What happened: " + reason);
-    })
 
     $: technologiesQuery = Promise.allSettled([]);
 
@@ -31,33 +37,42 @@
                 query: TECHNOLOGIES,
                 variables: variables
             }).then(r => r.data.technologies);
-            technologiesQuery.then(console.log);
         } else {
             technologiesQuery = Promise.allSettled([]);
         }
     }
 </script>
 
-<h1>Tech Radar</h1>
-<Form>
-    <FormGroup>
-        <Input type="select">
-            {#await fieldsQuery then fields}
-                {#each fields as field}
-                    <option>{field.name}</option>
-                {/each}
-            {/await}
-        </Input>
-        <Input type="text" on:input={event => searchTechnology(event.target.value)}/>
-        <ListGroup>
-            {#await technologiesQuery then technologies}
-                {#each technologies as technology}
-                    <ListGroupItem>{technology.name}</ListGroupItem>
-                {/each}
-            {/await}
-        </ListGroup>
-    </FormGroup>
-</Form>
+<Styles/>
+<div class="container">
+    <Row><Col sm={{ size: 6, order: 2, offset: 1 }}>
+        <Form>
+            <FormGroup>
+                <InputGroup>
+                    <InputGroupText><Icon name="bag" /></InputGroupText>
+                    <Input type="select">
+                        {#await fieldsQuery then fields}
+                            {#each fields as field}
+                                <option>{field.name}</option>
+                            {/each}
+                        {/await}
+                    </Input>
+                </InputGroup>
+                <InputGroup>
+                    <InputGroupText><Icon name="search" /></InputGroupText>
+                    <Input type="text" placeholder="Search technology to evaluate" on:input={event => searchTechnology(event.target.value)}/>
+                </InputGroup>
+                <ListGroup>
+                    {#await technologiesQuery then technologies}
+                        {#each technologies as technology}
+                            <ListGroupItem>{technology.name}</ListGroupItem>
+                        {/each}
+                    {/await}
+                </ListGroup>
+            </FormGroup>
+        </Form>
+    </Col></Row>
+</div>
 
 <style>
     @import "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css";
