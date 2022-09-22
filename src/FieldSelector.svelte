7757fee23fe97f13d471e37d6dab027c1d236027
Thor Åge Eldby <tel@knowit.no>
2022-09-22 15:41:01 +0200

    Use writable to communicate field changes

--- src/FieldSelector.svelte
+++ src/FieldSelector.svelte
@@ -1,20 +1,28 @@
 <script>
     import {Icon, Input, InputGroup, InputGroupText} from 'sveltestrap';
     import {gql} from "@apollo/client";
-    import {getContext} from "svelte";
+    import {getContext, setContext} from "svelte";
+    import {fieldIdWritable} from "./global";
 
     const client = getContext('graphql-client');
 
     const FIELDS = gql`query { technology_fields { id name description } }`;
     const fieldsQuery = client.query({ query: FIELDS }).then(r => r.data.technology_fields);
+
+    let selected = 1;
+
+    $: update(selected);
+    function update(id) {
+        fieldIdWritable.set(id);
+    }
 </script>
 
 <InputGroup>
     <InputGroupText><Icon name="bag" /></InputGroupText>
-    <Input type="select">
+    <Input bind:value={selected} type="select">
         {#await fieldsQuery then fields}
             {#each fields as field}
-                <option>{field.name}</option>
+                <option value={field.id}>{field.name}</option>
             {/each}
         {/await}
     </Input>
